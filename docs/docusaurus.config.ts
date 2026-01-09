eb8cba22f4f962f9c22af513e1ad0c5fcdacf24c
闲炎 <binxin.wbx@alibaba-inc.com>
2026-01-09 16:24:36 +0800

    feat: add GitHub action for docs
    Change-Id: I1c1bb1c7d67e631dbf9fc87d3f3ca4725cfdc640

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://infraguard.example.com',
+  url: 'https://aliyun.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/infraguard/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
